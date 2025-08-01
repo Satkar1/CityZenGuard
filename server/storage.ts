0ac90bd30e1c855644dbdabf123b29d67a4c34cc
Satkar <143149990+Satkar1@users.noreply.github.com>
2025-08-01 12:51:28 +0530

    Update storage.ts

--- server/storage.ts
+++ server/storage.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import pkg from "pg";
+const { Pool } = pkg;
 import { eq } from "drizzle-orm";
 import {
   type User, type InsertUser,
